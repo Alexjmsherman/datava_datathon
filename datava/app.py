b9f39bc568c42ec74b13803d5a8d049c8036fa27
Alexjmsherman <alexjmsherman@gmail.com>
2016-08-25 10:13:08 -0400

    set up routes and form for skills survey

--- datava/app.py
+++ datava/app.py
@@ -11,6 +11,8 @@ from flask_sqlalchemy import SQLAlchemy
 import googlemaps
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from wtforms.fields.html5 import IntegerRangeField
+from wtforms.validators import NumberRange
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -20,12 +22,38 @@ gmaps = googlemaps.Client(key='AIzaSyAb416HhO9hmtIBYR4Zl-zZ3QAF0m7nJGE')
 initial_location = None
 
 
+### import skill survey data
+skill_names = pd.read_csv(r'C:\Users\alsherman\Desktop\Programming\hackathon\skills_df.txt')
+skill_names_list = skill_names.columns[1:]
+
+
+### FORMS
+
+class Survey(Form):
+    """ Survey questions including all skills for a user to provide personal ratings  """
+    q0 = IntegerRangeField(label=skill_names_list[0], default=0,
+                           validators=[NumberRange(min=0, max=5)])
+
+
+### ROUTES
+
 @app.route('/')
 @app.route('/datathon/')
 def index():
     # initiate the map at the VA library - location of hackathon
     return render_template('index.html', users_address=json.dumps(initial_location))
 
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index/', methods=['GET', 'POST'])
+def index():
+    form = Survey()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            q0 = form.q0.data
+
+    return render_template('index.html', form=form)
 
 if __name__ == "__main__":
     app.debug = True
